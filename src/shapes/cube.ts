36763d64e0a7fb20ed04a536bb4b897fc6c9e087
Fernando Salazar <fslzrr@gmail.com>
2020-10-29 21:33:33 -0700

    :recycle: set cube to common size

--- src/shapes/cube.ts
+++ src/shapes/cube.ts
@@ -1,6 +1,6 @@
 import { BoxGeometry, Mesh, MeshLambertMaterial } from "three";
 
-const geometry = new BoxGeometry(100, 100, 100);
+const geometry = new BoxGeometry();
 const material = new MeshLambertMaterial({ color: 0x00ff00 });
 const cube = new Mesh(geometry, material);
 
